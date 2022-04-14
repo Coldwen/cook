dc76429b7b056d9e024be8dcbfb6a49903d71f1e
doubleLeng <1695108628@qq.com>
2022-04-14 22:55:14 +0800

    fix:跨域请求

--- src/main/java/com/cook/cook/controller/DefaultController.java
+++ src/main/java/com/cook/cook/controller/DefaultController.java
@@ -1,11 +1,13 @@
 package com.cook.cook.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin //所有域名均可访问该类下所有接口
 public class DefaultController {
 
     @GetMapping
